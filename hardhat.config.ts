38535cde36c40ad154f4e62da6e33c4383903824
Chenxing Li <ChenxingLi@users.noreply.github.com>
2023-10-09 16:06:00 +0800

    Set optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,23 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers:[{
+      version: "0.8.19",
+      settings: {
+        optimizer: {
+          enabled: true
+        }
+      }
+    },{
+      version: "0.5.17",
+      settings: {
+        optimizer: {
+          enabled: true
+        }
+      }
+    }]
+  }
 };
 
 export default config;
